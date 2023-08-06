f6d11c2ee91b7428b9266246365f9b5ee9e27012
Amar Ghandour <A.Ghandour@excp.sa>
2023-08-06 16:03:14 +0300

    add swagger

--- FormBuilder/Program.cs
+++ FormBuilder/Program.cs
@@ -24,6 +24,7 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -33,6 +34,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
